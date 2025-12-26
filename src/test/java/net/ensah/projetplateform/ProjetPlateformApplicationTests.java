b524398105e93091aeb51bc3ac30baaec1cc6e59
maroua-maliki <marouamal12@gmail.com>
2025-12-26 11:57:44 +0100

    Fix: Mock TacheRepository to bypass database in tests

--- src/test/java/net/ensah/projetplateform/ProjetPlateformApplicationTests.java
+++ src/test/java/net/ensah/projetplateform/ProjetPlateformApplicationTests.java
@@ -1,10 +1,9 @@
 package net.ensah.projetplateform;
 
+import net.ensah.projetplateform.repository.TacheRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
@@ -13,9 +12,12 @@ import org.springframework.test.context.TestPropertySource;
 })
 class ProjetPlateformApplicationTests {
 
+    // On crée un "faux" bean pour satisfaire les dépendances de vos services
+    @MockBean
+    private TacheRepository tacheRepository;
+
     @Test
     void contextLoads() {
-        // Le contexte va maintenant se charger sans chercher à se connecter à MySQL
+        // Le contexte va démarrer car tacheRepository est maintenant "simulé"
     }
-
 }
